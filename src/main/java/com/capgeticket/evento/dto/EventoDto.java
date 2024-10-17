9f0d7127866b48f50a881d0ec4729c077580ea8f
gsusag00 <jesus.ag00@gmail.com>
2024-10-17 16:13:54 +0200

    DTO finished

--- src/main/java/com/capgeticket/evento/dto/EventoDto.java
+++ src/main/java/com/capgeticket/evento/dto/EventoDto.java
@@ -1,42 +1,43 @@
 package com.capgeticket.evento.dto;
 
-import jakarta.persistence.*;
+import com.capgeticket.evento.model.Evento;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class EventoDto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "nombre", nullable = false, length = 255)
     private String nombre;
-
-    @Column(name = "descripcion", nullable = false, length = 255)
     private String descripcion;
-
-    @Column(name = "fechaEvento", nullable = false)
     private LocalDate fechaEvento;
-
-    @Column(name = "precioMinimo", nullable = false, precision = 10, scale = 2)
     private BigDecimal precioMinimo;
-
-    @Column(name = "precioMaximo", nullable = false, precision = 10, scale = 2)
     private BigDecimal precioMaximo;
-
-    @Column(name = "localidad", nullable = false, length = 255)
     private String localidad;
-
-    @Column(name = "nombreDelRecinto", nullable = false, length = 255)
     private String nombreDelRecinto;
-
-    @Column(name = "genero", nullable = false, length = 255)
     private String genero;
-
-    @Column(name = "mostrar", nullable = false)
     private Boolean mostrar;
+
+    public static EventoDto of(Evento evento) {
+        EventoDto eventoDto = new EventoDto();
+        eventoDto.setId(evento.getId());
+        eventoDto.setNombre(evento.getNombre());
+        eventoDto.setDescripcion(evento.getDescripcion());
+        eventoDto.setFechaEvento(evento.getFechaEvento());
+        eventoDto.setPrecioMinimo(evento.getPrecioMinimo());
+        eventoDto.setPrecioMaximo(evento.getPrecioMaximo());
+        eventoDto.setLocalidad(evento.getLocalidad());
+        eventoDto.setNombreDelRecinto(evento.getNombreDelRecinto());
+        eventoDto.setGenero(evento.getGenero());
+        eventoDto.setMostrar(evento.getMostrar());
+        return eventoDto;
+    }
+
+    public static List<EventoDto> of(List<Evento> eventos) {
+        return eventos.stream().map(EventoDto::of).collect(Collectors.toList());
+    }
 }
