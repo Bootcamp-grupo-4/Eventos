405985d0f0f4cbf53f2c6ac98a73e183a5ebc72c
Dani Martinez <daniel.martinez-perez@capgemini.com>
2024-10-24 13:41:31 +0200

    Method not supported

--- src/main/java/com/capgeticket/evento/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/com/capgeticket/evento/exception/CustomGlobalExceptionHandler.java
@@ -6,11 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 import java.text.SimpleDateFormat;
@@ -19,7 +19,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class CustomGlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(CustomGlobalExceptionHandler.class);
 
@@ -54,6 +54,31 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
 
         return new ResponseEntity<>(body, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, WebRequest request) {
+        logger.error("Método HTTP no soportado: {}", ex.getMessage());
+
+        // Crear un mensaje personalizado
+        StringBuilder message = new StringBuilder();
+        message.append("El método HTTP '").append(ex.getMethod()).append("' no está permitido para esta ruta. ");
+        message.append("Métodos soportados: ");
+        if (ex.getSupportedHttpMethods() != null) {
+            ex.getSupportedHttpMethods().forEach(m -> message.append(m + " "));
+        }
+
+        // Estructura personalizada del cuerpo de la respuesta
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date()));
+        body.put("status", HttpStatus.METHOD_NOT_ALLOWED.value());
+        body.put("error", "Método HTTP no permitido");
+        body.put("message", message.toString());
+        body.put("path", request.getDescription(false));
+
+        return new ResponseEntity<>(body, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
 
 
 }
